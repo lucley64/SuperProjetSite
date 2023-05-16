42b2d34dd015473a32788a33e8020c3d66c2f335
Achille Furger <furgerachi@cy-tech.fr>
2023-05-16 13:40:38 +0200

    g

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+
+?>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/accueil.css">
+    <link rel="shortcut icon" type="image/png" href="img/LogoDKD.png">
+    <title>That'a Challenge</title>
+</head>
+<body>
+    <?php
+        include "php/header.php";
+    ?>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,24 @@
+<head>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<body>
+    <header>
+        <div class="titre">
+            <h1> <a href="index.php">That'a Challenge !</a></h1>
+        </div>
+        <div class="menuHeader">
+            <table>
+                <tr>
+                    <th><a href="index.php"><button class="buttonHeader">Accueil</button></a></th>
+                    <th><a href=""><button class="buttonHeader ">Actualités</button></a></th>
+                    <th><a href=""><button class="buttonHeader">À venir</button></a></th>
+                    <th><a href="" ><button class="buttonHeader">Archives</button></a></th>
+                    <th><a href=""><button class="buttonHeader">IA Pau</button></a></th>
+                    <th><a href=""><button class="buttonHeader">Tah la barre de recherche</button></a></th>
+                    <th><a href="php_client/client.php"><button class="buttonHeader">Se connecter</button></a></th>
+                </tr>
+            </table>
+        </div>
+    </header>
+</body>
