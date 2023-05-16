ddd518b8c374d466cc9aea6ee2f391dcd63772b1
MaelTriquet <mael1triquet@gmail.com>
2023-05-16 10:36:08 +0200

    first code analysor

--- CodeAnalysor.java
+++ CodeAnalysor.java
@@ -1,14 +1,13 @@
 import java.io.File;
 import java.util.Scanner;
-public class CodeAnalysor
-{
-    public static void main(String[] args) throws Exception
-    {
-        File doc =
-          new File("C:\\Drive\\Learn.txt");
-        Scanner obj = new Scanner(doc);
+public class CodeAnalysor {
+    public CodeAnalysor(File inputFile_) throws FileNotFoundException {
+        File pythonFile =
+          inputFile_;
+        Scanner scanner = new Scanner(pythonFile);
 
-        while (obj.hasNextLine())
-            System.out.println(obj.nextLine());
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
         }
+    }
 }
