9ba071ecc4eb5360769f15e5b4c58dceda64f4b7
Diego <diegojedachercoles@gmail.com>
2023-05-17 10:18:35 +0200

    Début de l'accueil

--- index.php
+++ index.php
@@ -9,13 +9,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/accueil.css">
-    <link rel="shortcut icon" type="image/png" href="img/LogoDKD.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+
     <title>That'a Challenge</title>
 </head>
 <body>
     <?php
         include "php/header.php";
     ?>
+    <div class="principal">
+        <h2>Participez à des challenges avec l'association IA Pau</h2>
+
+        <p>L'association IA Pau propose de nombreux data challenges et data battles, inscrivez vous sur "that'a challenge" pour pouvoir gérer tous vos projets et en découvrir d'autres</p>
+    </div>
 </body>
 </html>
