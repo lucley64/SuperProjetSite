fc27b0874b18aac511599407bc39d912dde3485f
Diego <diegojedachercoles@gmail.com>
2023-05-17 14:30:23 +0200

    Petite animation barre laterale

--- php/barrecote.php
+++ php/barrecote.php
@@ -1,10 +1,10 @@
 
     <link rel="stylesheet" href="../css/barrecote.css">
-
+    <script type="text/javascript" src="js/barrecote.js"></script>
 <body>
-        <div class=barrecote>
-            <img src="src/liste.png" alt="">
-            <ul>
+        <div class=barrecote onmouseover="afficherbarre()" onmouseout="cacherbarre()">
+            <img src="src/liste.png" alt="iconeliste">
+            <ul id="liste">
                 <li>Menu 1</li>
                 <li>menu 2</li>
                 <li>menu 3</li>
