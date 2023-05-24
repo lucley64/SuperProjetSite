d8be19269b337b6728eab920f47e3ae681ffaf5f
Diego <diegojedachercoles@gmail.com>
2023-05-24 12:55:36 +0200

    stp

--- js/messagerie.js
+++ js/messagerie.js
@@ -2,7 +2,7 @@ async function NvMessages() {
     try {
       const response = await fetch('messagerie/messagesrecus.php');
       const data = await response.text();
-      console.log(data);
+      //console.log(data);
       document.getElementById("ContenuMessages").innerHTML=data;
       setTimeout(NvMessages, 5000);
     } catch (error) {
@@ -17,12 +17,22 @@ async function NvMessages() {
     try {
       const response = await fetch('messagerie/messagesEnvoyes.php');
       const data = await response.text();
-      console.log(data);
+      //console.log(data);
       document.getElementById("ContenuMessagesEnvoyes").innerHTML=data;
       setTimeout(MessagesEnvoyes, 5000);
     } catch (error) {
-      console.error('Erreur lors de la récupération des nouveaux messages:', error);
+      console.error('Erreur lors de la récupération des messages envoyés:', error);
     }
   }
 
 MessagesEnvoyes();
+
+function afficher(elt){
+    var vals=document.getElementsByClassName("elt");
+    /*on cache tout*/
+    for (let i = 0; i < vals.length; i++) {
+        vals[i].style.display="none";
+    }
+    let classe=elt.className;
+    document.getElementById(classe).style.display="block";
+}
