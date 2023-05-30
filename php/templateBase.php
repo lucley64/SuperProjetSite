c06fdde92cc27831da7d3b098cfe6820a7edc542
lucley64 <luc.leydert@gmail.com>
2023-05-30 14:14:44 +0200

    ajout template

---
+++ php/templateBase.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <img class="background" src="/src/pyrenees.jpg" alt="pyr">
+    <div id="sidebarr">
+        <?php
+        include "sidebar.php";
+        ?>
+    </div>
+    <div id="head">
+        <?php
+        include "header.php";
+        ?>
+
+    </div>
+    <div id="contenupage">
+        <!--!!! Contenu de la page ici !!!-->
+    </div>
+</body>
+
+</html>
